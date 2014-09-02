c32213779881552b98e040bb6f42e3590b19dc7a
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2014-09-02 11:12:33 +0900

    Make 1 test, 1 assertion: test/test-destroy.js

--- test/test-destroy.js
+++ test/test-destroy.js
@@ -30,57 +30,60 @@ var assert = require("power-assert");
 
 describe("ObserverSubject.destroy()", function(){
     var ObserverSubject = window.ObserverSubject;
-    var gSubject = null;
-    var gObserver = null;
 
-    beforeEach(function(){
-        gSubject = new ObserverSubject();
-        gObserver = {
-            handleMessage: function () {
-            }
-        };
-    });
+    describe("finalize the subject", function(){
+        var gSubject = null;
 
-    afterEach(function(){
-        gSubject = null;
-        gObserver = null;
-    });
-
-    it("finalize the subject", function(){
         var isCalled1 = true;
-        var o1 = {
-            handleMessage: function () {
-                isCalled1 = false;
-            }
-        };
-
         var isCalled2 = true;
-        var o2 = {
-            handleMessage: function () {
-                isCalled2 = false;
-            }
-        };
-
         var isCalled3 = true;
-        var o3 = {
-            handleMessage: function () {
-                isCalled3 = false;
-            }
-        };
 
-        gSubject.add("test1", o1);
-        gSubject.add("test2", o2);
-        gSubject.add("test3", o3);
+        before(function(){
+            gSubject = new ObserverSubject();
 
-        gSubject.destroy();
+            var o1 = {
+                handleMessage: function () {
+                    isCalled1 = false;
+                }
+            };
+            gSubject.add("test1", o1);
 
-        gSubject.notify("test1", null);
-        gSubject.notify("test2", null);
-        gSubject.notify("test3", null);
+            var o2 = {
+                handleMessage: function () {
+                    isCalled2 = false;
+                }
+            };
+            gSubject.add("test2", o2);
 
-        assert(isCalled2 === true, "should not call 'o1.handleMessage()' after the subject was destroyed.");
-        assert(isCalled2 === true, "should not call 'o2.handleMessage()' after the subject was destroyed.");
-        assert(isCalled2 === true, "should not call 'o3.handleMessage()' after the subject was destroyed.");
+            var o3 = {
+                handleMessage: function () {
+                    isCalled3 = false;
+                }
+            };
+            gSubject.add("test3", o3);
+
+            gSubject.destroy();
+
+            gSubject.notify("test1", null);
+            gSubject.notify("test2", null);
+            gSubject.notify("test3", null);
+        });
+
+        after(function(){
+            gSubject = null;
+        });
+
+        it("should not call 'o1.handleMessage()' after the subject was destroyed.", function () {
+            assert(isCalled1 === true);
+        });
+
+        it("should not call 'o2.handleMessage()' after the subject was destroyed.", function () {
+            assert(isCalled2 === true);
+        });
+
+        it("should not call 'o3.handleMessage()' after the subject was destroyed.", function () {
+            assert(isCalled3 === true);
+        });
     });
 });
 
