1f06f46bbdad7bd57b90efb8cf55f1c6fec83554
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2014-11-19 17:10:07 +0900

    0.6.1

--- observer.js
+++ observer.js
@@ -1,7 +1,7 @@
 /* vim: set filetype=javascript shiftwidth=4 tabstop=4 expandtab: */
 /**
  * https://github.com/saneyuki/observer-js
- * @version     0.6.0
+ * @version     0.6.1
  * @license     MIT License
  */
 /*
